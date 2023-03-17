32729da6ff6b9f9a140b8561f0238d3233155ad0
wangzaihehe <108859547+wangzaihehe@users.noreply.github.com>
2023-03-17 13:15:10 -0700

    Update fs.c

--- libfs/fs.c
+++ libfs/fs.c
@@ -39,9 +39,34 @@ struct file_descriptor {
 struct superblock superblock;
 struct root_directory root_directory;
 struct file_descriptor fd_table[FS_OPEN_MAX_COUNT];
-uint8_t bounce[BLOCK_SIZE];
+
 // helper functs
-int block_create(){
+
+uint8_t bounce[BLOCK_SIZE];
+#define error(fmt, ...) \
+	fprintf(stderr, "%s: "fmt"\n", __func__, ##__VA_ARGS__)
+
+#define fs_error(...)				\
+{							\
+	error(__VA_ARGS__);	\
+	return -1;					\
+}
+
+#define fs_perror(...)				\
+{							\
+	perror(__VA_ARGS__);	\
+	return -1;					\
+}
+uint16_t FAT_iterator(uint16_t block_index, uint16_t count)
+{
+	for (uint16_t i = 0; i < count; ++i) {
+		block_index = FAT[block_index];
+	}
+	return block_index;
+}
+
+
+int block_create(int fd){
 	int index = 0;
 	for (int i = 0; i < superblock.fat_amount * 2048; i++ ) {
 		if (FAT[i] == 0) {
@@ -52,8 +77,11 @@ int block_create(){
 			return -1;
 		}
 	}
+    fd_table[fd].entry->datablk_start_index = index;
+    FAT[index] = 0xFFFF;
 	return index;
 }
+
 int rdir_free_blocks() {
 	int counter = FS_FILE_MAX_COUNT;
 	for (int i = 0; i <FS_FILE_MAX_COUNT; i++) {
@@ -94,13 +122,21 @@ int fs_mount(const char *diskname)
 
 int fs_umount(void)
 {
+
 	block_write(superblock.rootdir_blk_index, &root_directory);
-	for (int i = 1; i < superblock.fat_amount + 1; ++i) {
-		block_write(i, &(FAT[i-1 * BLOCK_SIZE/2]));
+	for (int i = 0; i < superblock.fat_amount; ++i) {
+		block_write(i + 1, &(FAT[i * (BLOCK_SIZE/2)]));
+	}// 错
+    for (int i = 0; i < FS_OPEN_MAX_COUNT; ++i) {
+		if (fd_table[i].entry != NULL)
+			fs_error("There exist open file descriptors");
 	}
+
 	superblock = (const struct superblock){ 0 };
 	memset(FAT, 0, sizeof(FAT));
 	root_directory = (const struct root_directory){ 0 };
+    memset(bounce, 0, sizeof(bounce));
+
 	return 0;
 }
 
@@ -209,95 +245,82 @@ int fs_lseek(int fd, size_t offset)
 
 int fs_write(int fd, void *buf, size_t count)
 {
-	int first_dblock = 0;
-	struct file_descriptor temp_filedesc = fd_table[fd];
-	int file_in_direc = 0;
-	int remaining = count;
-	uint8_t bounce[BLOCK_SIZE];
-	int buffer_written = 0;
-	for (int i = 0 ; i < FS_FILE_MAX_COUNT ; i++) {
-		if (root_directory.entry_array[i].filename == fd_table[fd].entry->filename) {
-			first_dblock= root_directory.entry_array[i].datablk_start_index;
-			file_in_direc= i;
-		}
-	}
-	// create block if file is new
+	uint32_t total_written_count = 0;
+	uint16_t offset_in_one_block = fd_table[fd].offset % BLOCK_SIZE;
+	uint16_t current_block_index;
+	uint16_t iteration_written_count;
+    int finish_flag = 0;
+
+	/* Error Checking */
+	// Check if FS is mounted
+	if (superblock.signature != 0x5346303531534345)
+		fs_error("Filesystem not mounted");
+
+	// Check if file descriptor is closed or out of bounds
+	if (fd_table[fd].entry == NULL || fd >= FS_OPEN_MAX_COUNT)
+		fs_error("Invalid file descriptor");
+
+	if (buf == NULL)
+		fs_error("buf is NULL");
+
+	if (count == 0)
+		return 0;//必须要有
+
+	// checkif exist data block.
 	if (fd_table[fd].entry->datablk_start_index == 0xFFFF) {
-		int new_block_index = block_create();
-		if (new_block_index == -1) {
-			return 0; //no more space for creating a new block
-		}
-		root_directory.entry_array[file_in_direc].datablk_start_index = new_block_index;
-		first_dblock = new_block_index;
-		temp_filedesc.entry->datablk_start_index = new_block_index;
-		FAT[new_block_index] = 0xFFFF;
+		current_block_index = block_create(fd);
+	} else {
+		current_block_index = FAT_iterator(fd_table[fd].entry->datablk_start_index, fd_table[fd].offset / BLOCK_SIZE);
 	}
-	while (remaining > 0) {
-		int aimmed_index  = (temp_filedesc.offset / BLOCK_SIZE) + first_dblock + superblock.datablk_start_index;
-		int block_taken_index  = temp_filedesc.offset % BLOCK_SIZE;
-		block_read(aimmed_index,&bounce);
-		if (block_taken_index + remaining <= BLOCK_SIZE) {
-			memcpy(&bounce+block_taken_index, buf+buffer_written, remaining);
-			temp_filedesc.offset += remaining;
-			buffer_written += remaining;
-			remaining = 0;
-			block_write(aimmed_index,&bounce);
-		} else if (block_taken_index + remaining > BLOCK_SIZE) {
-			int block_remain = BLOCK_SIZE - block_taken_index;
-			memcpy(&bounce+block_taken_index, buf+buffer_written, block_remain);
-			temp_filedesc.offset += block_remain;
-			buffer_written += block_remain;
-			remaining = remaining - block_remain;
-			block_write(aimmed_index,&bounce);
-			int new_block_index = block_create();
-			FAT[aimmed_index] = new_block_index;
-			FAT[new_block_index] = 0xFFFF;
+    while (finish_flag != 1) {
+		if ( count - total_written_count >= (unsigned int)BLOCK_SIZE - offset_in_one_block) {
+				iteration_written_count = (unsigned int)BLOCK_SIZE - offset_in_one_block;
+		} else {
+				iteration_written_count = count - total_written_count;
 		}
-	}
+        //read whole block into bounce
+        block_read(current_block_index +superblock.datablk_start_index, &bounce );
+        //copy the aimed area of data into bounce correct position
+        memcpy(&bounce[offset_in_one_block], buf+total_written_count, iteration_written_count);
+        total_written_count += iteration_written_count;
+        //update file offset to the end of the current position
+        fd_table[fd].offset += iteration_written_count;
+        //write back bounce into datablock
+        block_write(current_block_index +superblock.datablk_start_index, &bounce );
+        //iterate through FAT[] or create new FAT entry
+		if (FAT[current_block_index] == 0xFFFF) {
+            int free_index;
+            for (int i = 1; i < superblock.fat_amount * (BLOCK_SIZE/2); i++) {
+                if (FAT[i] == 0){
+                    free_index = i;
+                    break;
+                }
+            }
+            FAT[current_block_index] = free_index;
+            FAT[free_index] = 0xFFFF;
+			current_block_index = free_index;
+		} else {
+			current_block_index = FAT_iterator(current_block_index, 1);
+		}
+        //since after 1st dblock, their offset are at the beginning of the block
+        offset_in_one_block = 0;
+        if(count - total_written_count == 0) {
+            finish_flag =1;
+        }
+    }
+	// update file size by using offset(end of the file)
 	if (fd_table[fd].entry->file_size < fd_table[fd].offset) {
 		fd_table[fd].entry->file_size = fd_table[fd].offset;
-	}
-	block_write(superblock.rootdir_blk_index, &root_directory);
-	for (int i = 1; i < superblock.fat_amount + 1; ++i) {
-		block_write(i, &(FAT[i-1 * BLOCK_SIZE/2]));
-	}
-	return buffer_written;
+    }    
+
+	return total_written_count;
 }
 
 int fs_read(int fd, void *buf, size_t count)
 {
-	/* TODO: Phase 4 */
-	int first_dblock = 0;
-	struct file_descriptor temp_filedesc = fd_table[fd];
-	int remaining = 0;
-	uint8_t bounce[BLOCK_SIZE];
-	int buffer_written = 0;
-	for (int i = 0 ; i < FS_FILE_MAX_COUNT ; i++) {
-		if (root_directory.entry_array[i].filename == fd_table[fd].entry->filename) {
-			first_dblock= root_directory.entry_array[i].datablk_start_index;
-		}
-	}
-	size_t size_offset = temp_filedesc.entry->file_size - temp_filedesc.offset;
-	if (size_offset >= count){
-		remaining = count;
-	} else {
-		remaining = size_offset;
-	}
-	while (remaining > 0) {
-		int aimmed_index  = (temp_filedesc.offset / BLOCK_SIZE) + first_dblock + superblock.datablk_start_index;
-		int block_taken_index  = temp_filedesc.offset % BLOCK_SIZE;
-		block_read(aimmed_index, &bounce);
-		if (block_taken_index + remaining <= BLOCK_SIZE) {
-			memcpy(buf + buffer_written, &bounce + block_taken_index, remaining);
-			temp_filedesc.offset += remaining;
-			buffer_written += remaining;
-			remaining = 0;
-		} else if (block_taken_index + remaining > BLOCK_SIZE) {
-			memcpy(buf + buffer_written, &bounce + block_taken_index, remaining);
-			temp_filedesc.offset += remaining;
-			buffer_written += remaining;
-			remaining = remaining - BLOCK_SIZE;			
-		}
-	}
-	return buffer_written;
+	uint32_t total_read_count = 0;
+	uint16_t offset_in_one_block = fd_table[fd].offset % BLOCK_SIZE;
+	uint16_t current_block_index;
+	uint16_t iteration_written_count;
+    int finish_flag = 0;
 }
