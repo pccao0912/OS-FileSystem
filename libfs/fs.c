c87cc238868a1694086cebe680be814feb0f3744
wangzaihehe <108859547+wangzaihehe@users.noreply.github.com>
2023-03-17 21:25:52 -0700

    Update fs.c

--- libfs/fs.c
+++ libfs/fs.c
@@ -386,6 +386,9 @@ int fs_write(int fd, void *buf, size_t count)
 					free_index = i;
 					break;
 				}
+				if (j==superblock.fat_amount * (BLOCK_SIZE/2)-1 && FAT[j] != 0) {
+					return total_written_count;
+				}
 			}
 			FAT[current_index] = free_index;
 			FAT[free_index] = 0xFFFF;
